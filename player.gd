67d0f2c1f0d26a7f5102df756cf98c956256f678
nickolasbradham <nickolasbradham@gmail.com>
2024-08-14 20:35:29 -0600

    Initial Commit

---
+++ player.gd
@@ -0,0 +1,14 @@
+extends CharacterBody2D
+
+const SPEED = 50
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	velocity.x = SPEED if Input.is_action_pressed("Right") else 0 - SPEED if Input.is_action_pressed("Left") else 0
+	velocity.y = SPEED if Input.is_action_pressed("Down") else 0 - SPEED if Input.is_action_pressed("Up") else 0
+	move_and_slide()
