630d717f43b29477601f8e5435d7d4627d92d2b2
nickolasbradham <nickolasbradham@gmail.com>
2024-09-16 19:13:42 -0600

    Godot update.

--- player.gd
+++ player.gd
@@ -8,7 +8,7 @@ func _ready():
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _process(_delta):
 	velocity.x = SPEED if Input.is_action_pressed("Right") else -SPEED if Input.is_action_pressed("Left") else 0
 	velocity.y = SPEED if Input.is_action_pressed("Down") else -SPEED if Input.is_action_pressed("Up") else 0
 	move_and_slide()
